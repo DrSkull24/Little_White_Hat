62b073c329090d7ddc8a4bd9fd34fb46e19dce8a
freville <noe.freville@ecole.ensicaen.fr>
2025-01-22 16:18:43 +0100

    adds zoom on object

--- firstGame/firstGame.js
+++ firstGame/firstGame.js
@@ -1,5 +1,7 @@
 let isCleared = false;
-let password = 'mdp'
+let password = 'mdp';
+let styleBeforeZoom;
+let currentScene = 'desktop';
 
 function changeScene(sceneId) {
     document.querySelectorAll('.scene').forEach(scene => {
@@ -13,11 +15,39 @@ function changeBackground(sceneId) {
     const body = document.body;
     if (sceneId === 'desktop') {
         body.style.backgroundImage = "url('img/desktop.jpeg')";
+        currentScene = 'desktop';
     } else if (sceneId === 'laptop') {
         body.style.backgroundImage = "none";
+        currentScene = 'laptop';
     } else if (sceneId === 'computer') {
         body.style.backgroundImage = "url('img/computer.png')";
+        currentScene = 'computer';
     }
+}
+
+function zoomOnObject(object) {
+    document.getElementById(currentScene).querySelectorAll('.object').forEach(otherObject => {
+        if (otherObject !== object) otherObject.style.display = 'none';
+    });
+    styleBeforeZoom = object.style;
+    object.style.position = 'absolute';
+    object.style.top = '25%';
+    object.style.left = '25%';
+    object.style.width = "50%";
+    object.style.height = "50%";
+    var backButton = document.getElementById('backButton');
+    backButton.style.display = 'block';
+    backButton.onclick = function() {
+        zoomOutObject(object);
+    };
+}
+
+function zoomOutObject(object) {
+    object.style = styleBeforeZoom;
+    document.getElementById(currentScene).querySelectorAll('.object').forEach(otherObject => {
+        otherObject.style.display = 'block';
+    });
+    document.getElementById('backButton').style.display = 'none';
 }
 
 function clearText(element) {
