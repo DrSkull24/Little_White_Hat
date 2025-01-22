daa19682ffaa71b0c3aaf2c7698ca8a1a701eef5
freville <noe.freville@ecole.ensicaen.fr>
2025-01-22 10:19:44 +0100

    changes between two interface

---
+++ secondInterface.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Second interface</title>
+        <link rel="stylesheet" href="firstInterface.css">
+        <script src="firstGame.js"></script>
+    </head>
+    <body>
+        <a href="firstInterface.php">
+            <img src="img/retour.jpeg">
+        </a>
+    </body>
+</html>
