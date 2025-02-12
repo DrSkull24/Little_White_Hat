c50cf47b0f42c89bca3981a40dec71b65c76ad53
freville <noe.freville@ecole.ensicaen.fr>
2025-02-12 11:29:02 +0100

    start page

--- start.js
+++ start.js
@@ -1,8 +1,19 @@
 window.onload = function() {
     let startButton = document.getElementById("startButton");
     startButton.style.position = 'absolute';
-    alert(startButton.width + ' ' + startButton.style.height);
-    startButton.style.top = window.innerHeight/2 - startButton.style.height + 'px';
-    startButton.style.left = window.innerWidth/2 - startButton.style.width + 'px';
-    alert(startButton.style.top + ' ' + startButton.style.left);
+    startButton.style.top = window.innerHeight - 2*startButton.offsetHeight + 'px';
+    startButton.style.left = window.innerWidth/2 - startButton.offsetWidth/2 + 'px';
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+    const text = "Bienvenue dans Little White Hat, un jeu d'investigation numérique. Serez-vous à la hauteur ?";
+    let index = 0;
+    function typeEffect() {
+        if (index < text.length) {
+            document.getElementById("intro-text").textContent += text.charAt(index);
+            index++;
+            setTimeout(typeEffect, 50);
+        }
+    }
+    typeEffect();
+});
